a213ad1b098f8cc65656be1f027420e00301f7d5
Erik <erik@Eriks-MacBook-Pro.local>
2026-03-29 09:48:34 -0700

    Update Hazmat.tsx

--- src/pages/services/Hazmat.tsx
+++ src/pages/services/Hazmat.tsx
@@ -20,8 +20,7 @@ const Hazmat = () => {
       >
         Skip to main content
       </a>
-      <LandstarSidebar />
-      <div className="md:ml-16">
+      <div>
         <Header />
         <main id="main-content">
           {/* Hero Section */}
