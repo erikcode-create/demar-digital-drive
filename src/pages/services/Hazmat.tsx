1d05cc049c3e48f348f5fdba5fc3b367d8074f13
Erik <erik@Eriks-MacBook-Pro.local>
2026-03-29 15:24:41 -0700

    Update Hazmat.tsx

--- src/pages/services/Hazmat.tsx
+++ src/pages/services/Hazmat.tsx
@@ -21,6 +21,35 @@ const Hazmat = () => {
       <div>
         <Header />
         <main id="main-content">
+          <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "BreadcrumbList",
+                "itemListElement": [
+                  {
+                    "@type": "ListItem",
+                    "position": 1,
+                    "name": "Home",
+                    "item": "https://demartransportation.com/"
+                  },
+                  {
+                    "@type": "ListItem",
+                    "position": 2,
+                    "name": "Services",
+                    "item": "https://demartransportation.com/"
+                  },
+                  {
+                    "@type": "ListItem",
+                    "position": 3,
+                    "name": "Hazmat Transportation",
+                    "item": "https://demartransportation.com/services/hazmat"
+                  }
+                ]
+              }),
+            }}
+          />
 
           {/* Hero */}
           <section className="pt-32 pb-20 px-4 bg-[hsl(225_97%_4%)] relative overflow-hidden">
