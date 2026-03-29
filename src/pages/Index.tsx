47c024edf8bbd66424e217dc5fa45bd3b88a38fe
Erik <erik@Eriks-MacBook-Pro.local>
2026-03-29 09:48:07 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,8 +13,7 @@ const Index = () => {
       >
         Skip to main content
       </a>
-      <LandstarSidebar />
-      <div className="md:ml-16">
+      <div>
         <Header />
         <main id="main-content">
           <Hero />
