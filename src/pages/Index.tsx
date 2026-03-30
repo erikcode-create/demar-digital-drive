b75a4d2ec7077692a7917b2c92a0df94431065de
Erik <erik@Eriks-MacBook-Pro.local>
2026-03-29 18:38:36 -0700

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
+import ResourcesPreview from "@/components/ResourcesPreview";
 import Footer from "@/components/Footer";
 
 const Index = () => {
